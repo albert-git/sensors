63eaa19b69e618ea862d149b2311fdfd070f8567
Albert <hdgcbtj@hotmail.com>
2018-09-05 09:22:59 +0800

    Create main2.ts

---
+++ main2.ts
@@ -0,0 +1,69 @@
+﻿/*
+Copyright (C): 2010-2019, Shenzhen Yahboom Tech
+modified from liusen
+load dependency
+"mbit": "file:../pxt-mbit"
+*/
+
+
+
+//% color="#C814B8" weight=25 icon="\uf1d4"
+
+/*****************************************************************************************************************************************
+ *  传感器类 ***************************************************************************************************************************** 
+ ****************************************************************************************************************************************/
+
+//% color="#87CEEB" weight=24 icon="\uf1b6"
+namespace mbit_传感器类 {
+
+    export enum enVoice {
+        //% blockId="Voice" block="有声音"
+        Voice = 0,
+        //% blockId="NoVoice" block="无声音"
+        NoVoice = 1
+    }
+
+    
+
+    //% blockId=mbit_Voice_Sensor block="Voice_Sensor|pin %pin|value %value"
+    //% weight=100
+    //% blockGap=10
+    //% color="#87CEEB"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function Voice_Sensor(pin: DigitalPin, value: enVoice): boolean {
+
+        pins.setPull(pin, PinPullMode.PullUp);
+        if (pins.digitalReadPin(pin) == value) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+    
+   
+    //% blockId=mbit_ultrasonic block="Ultrasonic|Trig %Trig|Echo %Echo"
+    //% color="#87CEEB"
+    //% weight=100
+    //% blockGap=10
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function Ultrasonic(Trig: DigitalPin, Echo: DigitalPin): number {
+
+        // send pulse
+        pins.setPull(Trig, PinPullMode.PullNone);
+        pins.digitalWritePin(Trig, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(Trig, 1);
+        control.waitMicros(15);
+        pins.digitalWritePin(Trig, 0);
+
+        // read pulse
+        let d = pins.pulseIn(Echo, PulseValue.High, 23200);
+        return d / 58;
+    }
+}
+
+
+
